d17019b2673f0e52e963136ef686d3606f3114cf
John Doe <dimahlynchak@gmail.com>
2024-10-01 17:11:33 +0300

    Add root endpoint for the Flask app

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
 from app import app
 
+@app.route('/')
+def index():
+    return "Welcome to my Flask App!", 200
+
 @app.route('/healthcheck')
 def healthcheck():
     return "Server is running!", 200
