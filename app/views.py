07be7345a944d618defda52f9d6697b5959cbdf0
John Doe <dimahlynchak@gmail.com>
2024-11-02 17:35:07 +0200

    Added endpoints for getting and deleting categories

--- app/views.py
+++ app/views.py
@@ -5,6 +5,9 @@ import uuid
 
 # Словник для зберігання даних про користувачів
 users = {}
+# Словник для зберігання даних про категорії
+categories = {}
+
 @app.route('/')
 def index():
     return "Welcome to my Flask App!", 200
@@ -38,3 +41,26 @@ def delete_user(user_id):
     if user is None:
         abort(404, description="User not found")
     return jsonify({"message": "User deleted successfully"}), 200
+
+@app.post('/category')
+def create_category():
+    category_data = request.get_json()
+    category_id = uuid.uuid4().hex
+    category = {"id": category_id, **category_data}
+    category[category_id] = category
+    return jsonify(category), 201
+
+@app.get('/category/category_id>')
+def get_category(category_id):
+    category = categories.get(category_id)
+    if category is None:
+        abort(404, description="Category not found")
+    return jsonify(category), 200
+
+@app.delete('/category/<category_id>')
+def delete_category(category_id):
+    category = categories.pop(category_id, None)
+    if category is None:
+        abort(404, description="Category not found")
+    return jsonify({"message": "Category deleted successfully"}), 200
+
